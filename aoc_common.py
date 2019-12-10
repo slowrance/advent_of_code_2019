55d9a005b284f642f2b8ffc77eaf8a0b9886dc6f
Steve Lowrance <steve.lowrance@gmail.com>
2019-12-09 18:51:41 -0800

    day05 part 1 complete

--- aoc_common.py
+++ aoc_common.py
@@ -5,3 +5,25 @@ def read_opcodes(filename):
     with open(filename) as f:
         opcodes += f.read().split(',')
     return opcodes
+
+def make_digits(num):
+    digits = [x for x in str(num)]
+    for idx, digit in enumerate(digits):
+        if digit == '-':
+            digits[idx] = 0
+            digits[idx + 1] = int(digits[idx + 1]) * -1
+    return digits
+
+def decode_digits(digits):
+    p1 = None
+    p2 = None
+    p3 = None
+    opcode = int(''.join(digits[-2:]))
+    if len(digits) > 2:
+        p1 = int(digits[-3])
+    if len(digits) > 3:
+        p2 = int(digits[-4])
+    if len(digits) > 4:
+        p3 = int(digits[-5])
+
+    return (opcode, (p1, p2, p3))
--- day05/day05.py
+++ day05/day05.py
@@ -1,44 +1,64 @@
-from aoc_common import read_opcodes
+from aoc_common import read_opcodes, make_digits, decode_digits
 
-
-initial = read_opcodes( 'day05_input.txt')
+initial = read_opcodes('day05_input.txt')
 
 
 def initialize():
     program = initial.copy()
-    program[1] = 1
-    return program
+    # for idx in program:
+    #     program[idx] = int(program[idx])
+    return [int(x) for x in program]
 
-def add_opcode(idx, program):
-    program[program[idx + 3]] = program[program[idx + 1]] + program[program[idx + 2]]
+def get_params(idx, program, param_flags):
+    p1 = program[idx + 1] if param_flags[0] else program[program[idx + 1]]
+    p2 = program[idx + 2] if param_flags[1] else program[program[idx + 2]]
+    p3 = program[idx + 3] if param_flags[2] else program[program[idx + 3]]
+    return p1, p2, p3
 
-def mul_opcode(idx, program):
-    program[program[idx + 3]] = program[program[idx + 1]] * program[program[idx + 2]]
+def add_opcode(idx, program, param_flags):
+    p1, p2, p3 = get_params(idx, program, param_flags)
+    # program[program[idx + 3]] = program[program[idx + 1]] + program[program[idx + 2]]
+    p3 = p1 + p2
+    program[program[idx + 3]] = p3
+
+
+def mul_opcode(idx, program, param_flags):
+    p1, p2, p3 = get_params(idx, program, param_flags)
+    # program[program[idx + 3]] = program[program[idx + 1]] * program[program[idx + 2]]
+    p3 = p1 * p2
+    program[program[idx + 3]] = p3
+
 
 def input_opcode(idx, program):
-    pass
+    program[program[idx + 1]] = 1
+
 
 def output_opcode(idx, program):
-    print(program[idx + 1])
-
-
+    print(program[program[idx + 1]])
 
 
 def run_program(program):
     idx = 0
     while idx < len(program):
-        if program[idx] == 1:
-            add_opcode(idx, program)
-        elif program[idx] == 2:
-            mul_opcode(idx, program)
-        elif program[idx] == 99:
+        opcode, param_flags = decode_digits(make_digits(program[idx]))
+        if opcode == 1:
+            add_opcode(idx, program, param_flags)
+            idx += 4
+        elif opcode == 2:
+            mul_opcode(idx, program, param_flags)
+            idx += 4
+        elif opcode == 3:
+            input_opcode(idx, program)
+            idx += 2
+        elif opcode == 4:
+            output_opcode(idx, program)
+            idx += 2
+        elif opcode == 99:
             break
-        idx += 4
 
     return program[0], program[1], program[2]
+
 
 if __name__ == '__main__':
     program = initialize()
     run_program(program)
-
-
